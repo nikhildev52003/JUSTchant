dc0904165a3d6831d24e2520a4d254da8c531b77
nikhildev52003 <nikhil.dev52003@gmail.com>
2025-11-08 16:15:23 +0530

    fix: correct case for Counter import

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { Counter } from './pages/counter.jsx'
-import './App.css'
+import { Counter } from './pages/Counter.jsx';
+import './App.css';
 
 function App() {
-  
   return (
     <>
-     <Counter/>
+      <Counter />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
